3eac4b9d6b3a8b26cd87b72c6b3b48f16bdd3f8a
Luis Santiago <luis.santiago@gmail.com>
2015-01-17 15:40:03 -0400

    4th commit

--- esspoket/Transaction.cs
+++ esspoket/Transaction.cs
@@ -11,19 +11,14 @@ namespace esspoket
         public Guid TransactionID { get; set; }
         public Guid RelatedTransactionID { get; set; }
         public DateTime TransactionDate { get; set; }
-
         public TransactionStatus TransactionStatus { get; set; }
         public Account OriginatingAccount { get; set; }
-
         public Account DestinationAccount { get; set; }
-
         public string Details { get; set; }
-
         public double OriginalAmount { get; set; }
-
         public double Fee { get; set; }
-
         public double NetAmount { get; set; }
-
+        public double CurrentBalance { get; set; }
+        public bool IsCurrentBalance { get; set; }
     }
 }
