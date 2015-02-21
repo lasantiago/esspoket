e181e44128b42dfd9164d14e72fdbe688f908ab3
Luis Santiago <luis.santiago@gmail.com>
2015-02-21 16:33:32 -0400

    Commited

---
+++ esspocketAPI/Controllers/AccountsController.cs
@@ -0,0 +1,44 @@
+﻿using esspocketORM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace esspocketAPI.Controllers
+{
+    public class AccountsController : ApiController
+    {
+        Account c = new Account();
+        public IEnumerable<Account> GetAll()
+        {
+            return c.GetAll(new EsspocketDBContext());
+        }
+
+        public Account GetAccountById(string id)
+        {
+            return c.GetAccountById(new EsspocketDBContext(), id);
+        }
+
+        public Account GetAccountByPhoneNumber(string id)
+        {
+            return c.GetAccountByPhoneNumber(new EsspocketDBContext(), id);
+        }
+
+        public Account GetAccountByEmail(string id)
+        {
+            return c.GetAccountByEmail(new EsspocketDBContext(), id);
+        }
+
+        public double GetAccountCurrentBalanceByAccountId(string id)
+        {
+            return c.GetAccountCurrentBalanceByAccountId(new EsspocketDBContext(), id);
+        }
+
+        public IEnumerable<Account> GetAccountsByLocalityId(string id)
+        {
+            return c.GetAccountsByLocalityId(new EsspocketDBContext(), id);
+        }
+    }
+}
---
+++ esspocketAPI/Controllers/TransactionsController.cs
@@ -0,0 +1,30 @@
+﻿using esspocketORM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace esspocketAPI.Controllers
+{
+    public class TransactionsController : ApiController
+    {
+        Transaction c = new Transaction();
+
+        public IEnumerable<Transaction> GetAllTransactions()
+        {
+            return c.GetAll(new EsspocketDBContext());
+        }
+
+        public Transaction GetTransactionById(string id)
+        {
+            return c.GetTransactionById(new EsspocketDBContext(), id);
+        }
+        
+        public int SubmitTransaction(string relatedtransactionid, int transactionstateid, int transactiontypeid, string accountid, string details, double originalamount, double fee, double netamount, double currentbalance, bool iscurrentbalance)
+        {
+            return c.SubmitTransaction(new EsspocketDBContext(), relatedtransactionid, transactionstateid, transactiontypeid, accountid, details, originalamount, fee, netamount, currentbalance, iscurrentbalance);
+        }
+    }
+}
